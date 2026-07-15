84ef6a91de946cd318c86980c0963eea5131d854
Recooky <381264252@qq.com>
2026-07-16 01:15:01 +0800

    Delete pages directory

--- pages/challenge.js
+++
@@ -1,5 +0,0 @@
-// 挑战模块 - 暂无专属逻辑
-function initChallengeModule() {
-    // 预留初始化逻辑，目前无操作
-}
-window.initChallengeModule = initChallengeModule;
--- pages/skill.js
+++
@@ -1,5 +0,0 @@
-// 技能模块 - 暂无专属逻辑
-function initSkillModule() {
-    // 预留初始化逻辑，目前无操作
-}
-window.initSkillModule = initSkillModule;
